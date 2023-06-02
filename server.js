3d08c577a71988585b53bbaf79e7010fd027c1fd
Błażej Kowol <kowol.blazej@gmail.com>
2023-06-02 23:24:42 +0200

    add mongoose connection & db

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const adsRoutes = require('./routes/ads.routes');
 const usersRoutes = require('./routes/users.routes');
 
@@ -23,3 +23,21 @@ app.use(express.urlencoded({ extended: false }));
 // add routes 
 app.use('/api', adsRoutes);
 app.use('/api', usersRoutes);
+
+// add db connection
+
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
+if(NODE_ENV === 'production') dbUri = 'url to remote db';
+else if(NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/adsAppDBtest';
+else dbUri = 'mongodb://localhost:27017/adsAppDB';
+
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+    
+db.once('open', () => {
+    console.log('Connected to the database');
+  });
+
+db.on('error', err => console.log('Error ' + err));     
